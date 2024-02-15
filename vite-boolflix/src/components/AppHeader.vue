25ab07ee79a4fa3ca66b17ab4e046e193cf544ca
Anna <annadalex@icloud.com>
2024-02-15 19:11:01 +0100

    Creazione input e button

--- vite-boolflix/src/components/AppHeader.vue
+++ vite-boolflix/src/components/AppHeader.vue
@@ -1,4 +1,5 @@
 <script >
+import {store} from '../store'
 export default{
     name:'AppHeader',
     data (){
@@ -10,10 +11,23 @@ export default{
 </script>
 
 <template>
-    <header class="p-3">
+    <header class="p-3 container d-flex justify-content-around align-item-center">
         <h1>
             Boolflix
         </h1>
+ 
+      <div class="d-flex align-items-center gap-4">
+        <label for="search" class="form-label">Cerca</label>
+         <input type="text"
+         class="form-control "
+         name=""
+         id="search"
+         placeholder="Cosa vuoi vedere?">
+         <button class="btn btn-secondary">
+          <i class="fas fa-magnifying-glass"></i>
+         </button>
+     </div>
+
     </header>
  
 </template>
--- vite-boolflix/src/store.js
+++ vite-boolflix/src/store.js
@@ -1,4 +1,10 @@
 import {reactive} from "vue";
 export const store= reactive({
-    
+    searchText:'',
+    movies:[],
+    series:[],
+    apiKey:'e1636b0170219715ac2faa25dc290353',
+    enpointMovies:'https://api.themoviedb.org/3/search/movie',
+    enpointSeries:'https://api.themoviedb.org/3/search/tv'
+
 })
