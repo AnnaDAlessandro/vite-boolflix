3442d6287fae456543c0269388afbe29b124d544
Anna <annadalex@icloud.com>
2024-02-16 21:23:47 +0100

    Aggiunta flags

--- vite-boolflix/src/components/AppMain.vue
+++ vite-boolflix/src/components/AppMain.vue
@@ -1,18 +1,35 @@
 
 <script >
+import { store } from '../store';
 import SingleCard from './SingleCard.vue';
 export default{
     name: 'AppMain',
     data() {
-        return {};
+        return {store};
     },
     components: { SingleCard }
 }
 </script>
 
 <template>
-    <div>
-       <SingleCard></SingleCard>  
+    
+    <div class="p-5">
+        <h2>Films</h2>
+        <div class="row">
+          <SingleCard
+          v-for="(movie,index) in store.movies"
+          :key="index"
+          :item="movie"> </SingleCard>   
+        </div>
+        <h2>Series</h2> 
+      <div class="row"> 
+       <SingleCard
+       v-for="(serie,index) in store.series"
+       :key="index"
+       :item="serie"></SingleCard>
+    </div>
+       
+
     </div>
 
 </template>
